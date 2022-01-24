1317ba09b27fb0a1865d0ebda153995f65b5f3e6
Lewin Kelly <heli@odex.cf>
2022-01-24 02:24:13 +0000

    IT FINALLY WORKS. adedd all pages, no images

--- src/App.svelte
+++ src/App.svelte
@@ -7,14 +7,5 @@
 </style>
 
 <body>
-	<div class="sidenav">
-		<!-- Page side navigation buttons go in this div -->
-
-		<img src="/heliodex.png" alt="Heliodex Logo" width="80" height="80">
-	</div>
-
-	
-	<div class="main">
-		<Router/>
-	</div>
+	<Router />
 </body>
--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,11 +1,32 @@
 <script>
-	import { Link } from 'svelte-routing';
+	import { link } from 'svelte-routing';
 </script>
 
-<div>
-	<h3>This is About Page</h3>
-	<Link class="headerbutton" To="/">Home</Link>
-	<Link class="headerbutton" To="projects">Projects</Link>
-	<Link class="headerbutton" To="about">About</Link>
-	<Link class="headerbutton" To="contact">Contact</Link>
+<style lang="sass">
+	@import src/style.sass
+</style>
+
+<div class="sidenav" >
+	<!-- Page side navigation buttons go in this div -->
+
+	<img src="/heliodex.png" alt="Heliodex Logo" width="80" height="80">
+
+	<a use:link class="headerbutton" href="/">Home</a>
+	<a use:link class="headerbutton" href="projects">Projects</a>
+	<a use:link class="headerbutton" href="about">About</a>
+	<a use:link class="headerbutton" href="contact">Contact</a>
+</div>
+
+<div class="main">
+	<h1>About</h1>
+	<br>
+
+	<p>Heliodex</p><br>
+	<p>/'hiliə</p><p class="ipa">ʊ</p><p>d</p><p class="ipa">ɛ</p><p>ks/</p><br>
+	<!-- Mix and match letters with the nearest looking font for phonetic characters. Not a good way of doing it. -->
+
+	<p>
+	text go here<br>
+	todo: about page
+	</p>
 </div>
