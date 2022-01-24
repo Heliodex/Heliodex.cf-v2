4e34344576d4d747b518dfc1a56fcc0326565290
Lewin Kelly <heli@odex.cf>
2022-01-24 14:42:47 +0000

    The site works!!!!!

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,21 +1,13 @@
 <script>
 	import { link } from 'svelte-routing';
+	import Sidenav from './Sidenav.svelte';
 </script>
 
 <style lang="sass">
 	@import src/style.sass
 </style>
 
-<div class="sidenav">
-	<!-- Page side navigation buttons go in this div -->
-
-	<img src="/heliodex.png" alt="Heliodex Logo" width="80" height="80">
-
-	<a use:link class="headerbutton" href="/">Home</a>
-	<a use:link class="headerbutton" href="projects">Projects</a>
-	<a use:link class="headerbutton" href="about">About</a>
-	<a use:link class="headerbutton" href="contact">Contact</a>
-</div>
+<Sidenav />
 
 <div class="main">
 	<h1>Heliodex.cf</h1>
