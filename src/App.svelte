cad72b96aecc79fd5eace932c8abf8cd51b5dc46
Lewin Kelly <heli@odex.cf>
2022-01-25 00:41:12 +0000

    added dynamically changing pages!!!

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,14 @@
 <script>
+	import Home from "./components/Home.svelte";
+	import Projects from "./components/Projects.svelte"; 
+	import About from "./components/About.svelte";
+	import Contact from "./components/Contact.svelte";
+
+	let Page = Home;
+	
+	function changePage(whichPage) {
+		Page = whichPage;
+	}
 </script>
 
 <style lang="sass">
@@ -11,10 +21,11 @@
 
 		<img src="/heliodex.png" alt="Heliodex Logo" width="80" height="80">
 
-		<button class="sideButton" href="/">Home</button>
-		<button class="sideButton" href="projects">Projects</button>
-		<button class="sideButton" href="about">About</button>
-		<button class="sideButton" href="contact">Contact</button>
+		<button class="sideButton" on:click={() => changePage(Home)}>Home</button>
+		<button class="sideButton" on:click={() => changePage(Projects)}>Projects</button>
+		<button class="sideButton" on:click={() => changePage(About)}>About</button>
+		<button class="sideButton" on:click={() => changePage(Contact)}>Contact</button>
 	</div>
-	
+
+	<svelte:component this={Page} />
 </body>
