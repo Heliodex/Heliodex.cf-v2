c0c0d26c74bb941504c0813bf60f6d616f9ed618
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2022-06-04 07:26:51 +0100

    Move from Rollup to Vite with Typescript

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,41 @@
-<script>
-	import Home from "./components/Home.svelte";
-	import Projects from "./components/Projects.svelte"; 
-	import About from "./components/About.svelte";
-	import Contact from "./components/Contact.svelte";
+<script defer lang="ts">
+	import Home from "./lib/Home.svelte"
+	import Projects from "./lib/Projects.svelte"
+	import About from "./lib/About.svelte"
+	import Contact from "./lib/Contact.svelte"
 
-	const pageDict = {"": Home, "projects": Projects, "about": About, "contact": Contact}; // Yanderedev lookin
+	const pageDict = {
+		"": Home,
+		projects: Projects,
+		about: About,
+		contact: Contact,
+	} // Yanderedev lookin
 	// it cannot be the other way around, with Home: "#home"
 
 	function getKeyByValue(object, value) {
-		return Object.keys(object).find(key => object[key] === value);
+		return Object.keys(object).find(key => object[key] === value)
 	}
 
-	let Page = pageDict[window.location.hash.substring(1)]; 
-	
+	let Page = pageDict[window.location.hash.substring(1)]
+
 	function changePage(whichPage) {
-		Page = whichPage; // If done in the same line, the svelte:component will not react
-		window.location.hash = getKeyByValue(pageDict, whichPage);
+		Page = whichPage // If done in the same line, the svelte:component will not react
+		window.location.hash = getKeyByValue(pageDict, whichPage)
 	}
 </script>
 
-<style lang="sass">
-	@import src/style.sass
-</style>
-
 <body>
-	<div class="sidenav" >
+	<div class="sidenav">
 		<!-- Page side navigation buttons go in this div -->
 
-		<img src="/heliodex.png" alt="Heliodex Logo" width="80" height="80">
+		<img class="logo" src="src/assets/heliodex.svg" alt="Heliodex Logo" width="80" height="80" />
 
 		<button class="sideButton" on:click={() => changePage(Home)}>Home</button>
 		<button class="sideButton" on:click={() => changePage(Projects)}>Projects</button>
 		<button class="sideButton" on:click={() => changePage(About)}>About</button>
 		<button class="sideButton" on:click={() => changePage(Contact)}>Contact</button>
 
-		<p class="version">v2.0.1</p>
+		<p class="version">v2.0.2</p>
 	</div>
 
 	<svelte:component this={Page} />
---
+++ src/lib/Contact.svelte
@@ -0,0 +1,25 @@
+<div class="main">
+	<h1>Contact</h1>
+	<br />
+
+	<p>
+		Here's a list of a few of the websites/media that you are able to contact me via.<br /><br />
+		Github: @Heliodex,
+		<a class="realLink" href="https://github.com/Heliodex/" target="_blank" rel="noopener noreferrer">GitHub.com/Heliodex</a><br />
+		Twitter: @lwinklly,
+		<a class="realLink" href="https://twitter.com/lwinklly/" target="_blank" rel="noopener noreferrer">Twitter.com/lwinklly</a><br />
+		Reddit: u/Heliodex,
+		<a class="realLink" href="https://www.reddit.com/user/Heliodex/" target="_blank" rel="noopener noreferrer">Reddit.com/u/Heliodex</a><br />
+		Developer Forum: Lewin4,
+		<a class="realLink" href="https://devforum.roblox.com/u/lewin4/summary/" target="_blank" rel="noopener noreferrer">Devforum.Roblox.com/u/lewin4</a><br />
+		Roblox: @Lewin4,
+		<a class="realLink" href="https://www.roblox.com/users/77663253/profile/" target="_blank" rel="noopener noreferrer">Roblox.com/users/77663253/profile</a><br />
+		Youtube: @Heliodex,
+		<a class="realLink" href="https://www.youtube.com/channel/UC8E2hw963cW5IJXZnBE6_vg/" target="_blank" rel="noopener noreferrer">YouTube.com/channel/UC8E2hw963cW5IJXZnBE6_vg</a><br />
+		Discord: Heliodex#3590,
+		<a class="realLink" href="https://discord.com/users/290622468547411968" target="_blank" rel="noopener noreferrer">Discord.com/users/290622468547411968</a><br />
+		Telegram: @Heliodex<br />
+		Email: Heli@odex.cf,
+		<a class="realLink" href="mailto:Heli@odex.cf" target="_blank" rel="noopener noreferrer">Heli@odex.cf</a><br />
+	</p>
+</div>
