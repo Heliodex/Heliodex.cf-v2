5df8e95196676a13f4a5253671d2248484b1189a
Lewin Kelly <heli@odex.cf>
2022-06-05 07:20:48 +0100

    Move sass into component sass

--- src/App.svelte
+++ src/App.svelte
@@ -24,19 +24,72 @@
 	}
 </script>
 
-<body>
+<main>
 	<div class="sidenav">
 		<!-- Page side navigation buttons go in this div -->
 
 		<img class="logo" src="src/assets/heliodex.svg" alt="Heliodex Logo" width="80" height="80" />
 
-		<button class="sideButton" on:click={() => changePage(Home)}>Home</button>
-		<button class="sideButton" on:click={() => changePage(Projects)}>Projects</button>
-		<button class="sideButton" on:click={() => changePage(About)}>About</button>
-		<button class="sideButton" on:click={() => changePage(Contact)}>Contact</button>
+		<button class="sideButton" on:mousedown={() => changePage(Home)}>Home</button>
+		<button class="sideButton" on:mousedown={() => changePage(Projects)}>Projects</button>
+		<button class="sideButton" on:mousedown={() => changePage(About)}>About</button>
+		<button class="sideButton" on:mousedown={() => changePage(Contact)}>Contact</button>
 
 		<p class="version">v2.0.2</p>
 	</div>
 
 	<svelte:component this={Page} />
-</body>
+</main>
+
+<style lang="sass">
+	.sidenav
+		height: 100%
+		width: 85px // Sidebar width
+		position: fixed // Stay in place when scrolling
+		z-index: 1 // Stay on top
+		top: 0 //Stay at the top
+		left: 0
+		background-color: #202020
+		overflow-x: hidden // Disable horizontal scroll
+		padding-top: 20px
+		padding-left: 5px
+
+	.logo
+		padding: 0 5px
+		width: 70px
+
+	.sideButton // Stolen straight from DocSocial lmao
+		border: none
+		background-color: #404040
+		margin: 4px 5px
+		border-radius: 5px
+		font-family: lexendDeca
+		font-size: 14px
+		padding: 5px 0 20px 0
+		height: 20px
+		cursor: pointer
+		color: #e3e3e3
+		width: 70px
+		display: inline-block
+
+		text-align: center
+		text-decoration: none
+
+		&:hover
+			transition: transform 0.2s
+			transform: scale(1.1)
+			background-color: #303030
+
+	.version
+		text-align: center
+		display: inline-block
+		margin: auto
+
+		position: fixed
+		bottom: 0 // fix to bottom of page
+		left: 0 // be inside rather than outside the sidebar
+		width: 70px
+
+	main
+		margin: 0 0 15px 100px // Width of the sidebar +15
+</style>
--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -1,118 +1,124 @@
-<div class="main">
-	<h1>Projects</h1>
-	<br />
-	<p>
-		Here's a list of all the projects that I have worked on, or the teams I have worked in.<br />
-	</p>
-	<br />
-	<br />
-	<div class="projects">
-		<div class="project">
-			<h3>DocSocial</h3>
+<h1>Projects</h1>
+<br />
+<p>
+	Here's a list of all the projects that I have worked on, or the teams I have worked in.<br />
+</p>
+<br />
+<br />
+<div class="projects">
+	<div>
+		<h3>DocSocial</h3>
 
-			<p>
-				Docsocial is a free, secure messaging application. It is designed to appear like a Google document from a quick glance, or from a look at your browser history.<br />
-				<br />
-				DocSocial uses
-				<a class="realLink" href="https://Scaledrone.com/" target="_blank" rel="noopener noreferrer">Scaledrone</a>
-				for its backend and for transferring messages.<br />
-				<br />
-				It was designed to work best with one specific device and OS, our school-assigned chromebooks, so pupils could chat with others in class and not be caught by a teacher.<br />
-				<a class="realLink" href="https://docsocial.tk/" target="_blank" rel="noopener noreferrer">DocSocial.tk</a><br />
-				<a class="realLink" href="https://docsocial.cf/" target="_blank" rel="noopener noreferrer">DocSocial.cf</a><br />
-				<br />
+		<p>
+			Docsocial is a free, secure messaging application. It is designed to appear like a Google document from a quick glance, or from a look at your browser history.<br />
+			<br />
+			DocSocial uses
+			<a class="realLink" href="https://Scaledrone.com/" target="_blank" rel="noopener noreferrer">Scaledrone</a>
+			for its backend and for transferring messages.<br />
+			<br />
+			It was designed to work best with one specific device and OS, our school-assigned chromebooks, so pupils could chat with others in class and not be caught by a teacher.<br />
+			<a class="realLink" href="https://docsocial.tk/" target="_blank" rel="noopener noreferrer">DocSocial.tk</a><br />
+			<a class="realLink" href="https://docsocial.cf/" target="_blank" rel="noopener noreferrer">DocSocial.cf</a><br />
+			<br />
 
-				<img src="src/assets/docsocial.png" alt="DocSocial Logo" width="300" />
-			</p>
-		</div>
-		<div class="project">
-			<h3>HybridOS</h3>
+			<img src="src/assets/docsocial.png" alt="DocSocial Logo" width="300" />
+		</p>
+	</div>
+	<div>
+		<h3>HybridOS</h3>
 
-			<p>
-				HybridOS was an open-source Python3 'meta-program'.<br />
-				I avoid calling it an operating system, because... it's not an operating system, it was written in Python.<br />
-				<br />
-				HybridOS was worked on from mid- to late-2019 as a fusion of two similar programs, BlakBird by me, and Vortex by Taskmanager. (<a
-					class="realLink"
-					href="https://taski.ml/"
-					target="_blank"
-					rel="noopener noreferrer">taski.ml</a
-				>)<br />
-				The program has a total line count of more than 1,100, and had features such as a multi-user signup and login system, a number of games and useful applications, plenty of colourful ASCII
-				art, and a root admin system.<br />
-				HybridOS was ported to many different languages, including C, C++, Java, C# (twice), and Batch. Even linux distros were made as well!<br />
-				<br />
-				HybridOS was largely abandoned after the beginning of 2020, and the code for the first Python version has been released under The Unlicense on my GitHub page.<br />
-				<a class="realLink" href="https://github.com/HelioDex/HybridOS/" target="_blank" rel="noopener noreferrer">Github.com/Heliodex/HybridOS</a>
-				<br />
-				<a class="realLink" href="https://www.youtube.com/watch?v=QwhaqpOL310/" target="_blank" rel="noopener noreferrer">Something big may be coming soon, though...</a><br />
-				<br />
-				<img src="src/assets/hylogos.png" alt="6 different HybridOS Logos throughout the versions" width="300" />
-			</p>
-		</div>
+		<p>
+			HybridOS was an open-source Python3 'meta-program'.<br />
+			I avoid calling it an operating system, because... it's not an operating system, it was written in Python.<br />
+			<br />
+			HybridOS was worked on from mid- to late-2019 as a fusion of two similar programs, BlakBird by me, and Vortex by Taskmanager. (<a class="realLink" href="https://taski.ml/" target="_blank" rel="noopener noreferrer">taski.ml</a>)<br />
+			The program has a total line count of more than 1,100, and had features such as a multi-user signup and login system, a number of games and useful applications, plenty of colourful ASCII art, and a root admin system.<br />
+			HybridOS was ported to many different languages, including C, C++, Java, C# (twice), and Batch. Even linux distros were made as well!<br />
+			<br />
+			HybridOS was largely abandoned after the beginning of 2020, and the code for the first Python version has been released under The Unlicense on my GitHub page.<br />
+			<a class="realLink" href="https://github.com/HelioDex/HybridOS/" target="_blank" rel="noopener noreferrer">Github.com/Heliodex/HybridOS</a>
+			<br />
+			<a class="realLink" href="https://www.youtube.com/watch?v=QwhaqpOL310/" target="_blank" rel="noopener noreferrer">Something big may be coming soon, though...</a><br />
+			<br />
+			<img src="src/assets/hylogos.png" alt="6 different HybridOS Logos throughout the versions" width="300" />
+		</p>
+	</div>
 
-		<div class="project">
-			<h3>PF Range Calculator</h3>
+	<div>
+		<h3>PF Range Calculator</h3>
 
-			<p>
-				This is a small web tool designed to calculate how far weapons in the game Phantom Forces by StyLiS Studios deal a certain amount of damage to.<br />
-				You can input some values provided by the in-game statistics, and the tool will calculate how far it will deal that damage to.<br />
-				<br />
-				This was created by me in a few days, after I realised how long it would take to calculate all the range values manually.<br />
-				I remade the site a few months later, being my first time using Svelte.<br />
-				You can find the calculator at
-				<a class="realLink" href="https://pfcalc.heliodex.cf/" target="_blank" rel="noopener noreferrer">PFCalc.Heliodex.cf</a>
-				.<br />
-				The older version can be seen at
-				<a class="realLink" href="https://oldpfcalc.heliodex.cf/" target="_blank" rel="noopener noreferrer">OldPFCalc.Heliodex.cf</a>
-				.<br />
-				<br />
+		<p>
+			This is a small web tool designed to calculate how far weapons in the game Phantom Forces by StyLiS Studios deal a certain amount of damage to.<br />
+			You can input some values provided by the in-game statistics, and the tool will calculate how far it will deal that damage to.<br />
+			<br />
+			This was created by me in a few days, after I realised how long it would take to calculate all the range values manually.<br />
+			I remade the site a few months later, being my first time using Svelte.<br />
+			You can find the calculator at
+			<a class="realLink" href="https://pfcalc.heliodex.cf/" target="_blank" rel="noopener noreferrer">PFCalc.Heliodex.cf</a>
+			.<br />
+			The older version can be seen at
+			<a class="realLink" href="https://oldpfcalc.heliodex.cf/" target="_blank" rel="noopener noreferrer">OldPFCalc.Heliodex.cf</a>
+			.<br />
+			<br />
 
-				<img src="src/assets/pfcalc.png" alt="Screenshot of the calculator page" width="300" />
-			</p>
-		</div>
+			<img src="src/assets/pfcalc.png" alt="Screenshot of the calculator page" width="300" />
+		</p>
+	</div>
 
-		<div class="project">
-			<h3>Crazy Calculations</h3>
+	<div>
+		<h3>Crazy Calculations</h3>
 
-			<p>
-				Crazy Calculations was a simple maths game that would test you through 10 levels of increasingly difficult maths questions.<br />
-				It was programmed in about a week for a Computer Science class assignment, where a video had to be recorded explaining the project.<br />
-				The project was started on 23 September 2021, and submitted on 4 October 2021.<br />
-				<br />
-				The game was inspired by (and much of the code ported from)
-				<a class="realLink" href="https://www.tomscott.com/usvsth3m/maths/" target="_blank" rel="noopener noreferrer">You Can't Do Simple Maths Under Pressure</a>
-				by UsVsTh3m.<br />
-				<br />
-				Crazy Calculations is playable at
-				<a class="realLink" href="https://www.roblox.com/games/7557207546/" target="_blank" rel="noopener noreferrer">Roblox.com/games/7557207546</a>
-				.<br />
-				<br />
+		<p>
+			Crazy Calculations was a simple maths game that would test you through 10 levels of increasingly difficult maths questions.<br />
+			It was programmed in about a week for a Computer Science class assignment, where a video had to be recorded explaining the project.<br />
+			The project was started on 23 September 2021, and submitted on 4 October 2021.<br />
+			<br />
+			The game was inspired by (and much of the code ported from)
+			<a class="realLink" href="https://www.tomscott.com/usvsth3m/maths/" target="_blank" rel="noopener noreferrer">You Can't Do Simple Maths Under Pressure</a>
+			by UsVsTh3m.<br />
+			<br />
+			Crazy Calculations is playable at
+			<a class="realLink" href="https://www.roblox.com/games/7557207546/" target="_blank" rel="noopener noreferrer">Roblox.com/games/7557207546</a>
+			.<br />
+			<br />
 
-				<img src="src/assets/crazycalc.png" alt="Screenshot of the game" width="300" />
-			</p>
-		</div>
+			<img src="src/assets/crazycalc.png" alt="Screenshot of the game" width="300" />
+		</p>
+	</div>
 
-		<div class="project">
-			<h3>Python Calculator Collection</h3>
+	<div>
+		<h3>Python Calculator Collection</h3>
 
-			<p>
-				This is a collection of calculator programs I wrote on my calculator's python interpreter during maths classes.<br />
-				There are programs for calculating interest over time, areas or volumes of polytopes, angles of arcs of circles, and much more.<br />
-				<br />
-				I cannot and will not guarantee the accuracy of these programs, but while they <em>should</em> be decently accurate, please submit an issue to the repo if you find an error in any of
-				the programs.<br />
-				They are available at
-				<a class="realLink" href="https://github.com/Heliodex/PythonCalculators/" target="_blank" rel="noopener noreferrer">GitHub.com/Heliodex/PythonCalculators</a>
-				.<br />
-			</p>
-		</div>
+		<p>
+			This is a collection of calculator programs I wrote on my calculator's python interpreter during maths classes.<br />
+			There are programs for calculating interest over time, areas or volumes of polytopes, angles of arcs of circles, and much more.<br />
+			<br />
+			I cannot and will not guarantee the accuracy of these programs, but while they <em>should</em> be decently accurate, please submit an issue to the repo if you find an error in any of the programs.<br />
+			They are available at
+			<a class="realLink" href="https://github.com/Heliodex/PythonCalculators/" target="_blank" rel="noopener noreferrer">GitHub.com/Heliodex/PythonCalculators</a>
+			.<br />
+		</p>
+	</div>
 
-		<div class="project">
-			<h3>Heliodex.cf</h3>
+	<div>
+		<h3>Heliodex.cf</h3>
 
-			<p>The website you're browsing right now!</p>
-			<iframe title="this website" src="/" width="80%" height="200%" frameborder="0" />
-		</div>
+		<p>The website you're browsing right now!</p>
+		<iframe title="this website" src="/" width="80%" height="200%" frameborder="0" />
 	</div>
 </div>
+
+<style lang="sass">
+	// Page dual-column code
+	@media screen and (min-width: 800px)
+		.projects 
+			display: grid
+			grid-template-columns: auto auto
+
+	h3
+		font-family: lexendDeca, sans-serif
+
+	div
+		grid-column-gap: 100px
+		padding-right: 5%
+</style>
