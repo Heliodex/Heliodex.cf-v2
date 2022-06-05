3ca42bbbada6d56527111bb37ff67c7f299d9d1c
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2022-06-05 07:20:48 +0100

    Move sass into component sass

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -1,19 +1,22 @@
-<div class="main">
-	<h1>About</h1>
-	<br />
+<h1>About</h1>
+<br />
 
-	<p>Heliodex</p>
-	<br />
-	<p>/'hiliə</p>
-	<p class="ipa">ʊ</p>
-	<p>d</p>
-	<p class="ipa">ɛ</p>
-	<p>ks/</p>
-	<br />
-	<!-- Mix and match letters with the nearest looking font for phonetic characters. Not a good way of doing it. -->
+<p>Heliodex</p>
+<br />
+<p>/'hiliə</p>
+<p class="ipa">ʊ</p>
+<p>d</p>
+<p class="ipa">ɛ</p>
+<p>ks/</p>
+<br />
+<!-- Mix and match letters with the nearest looking font for phonetic characters. Not a good way of doing it. -->
 
-	<p>
-		text go here<br />
-		todo: about page
-	</p>
-</div>
+<p>
+	text go here<br />
+	todo: about page
+</p>
+
+<style lang="sass">
+	.ipa
+		font-family: Helvetica, Arial, sans-serif // support for phonetic chars
+</style>
--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -1,8 +1,6 @@
-<div class="main">
-	<h1>Heliodex.cf</h1>
-	<p>
-		<br />
-		Welcome to Heliodex.cf!<br />
-		This is my portfolio page where you can see the projects that I've worked on, the teams that I've worked in, and how to contact me.<br />
-	</p>
-</div>
+<h1>Heliodex.cf</h1>
+<p>
+	<br />
+	Welcome to Heliodex.cf!<br />
+	This is my portfolio page where you can see the projects that I've worked on, the teams that I've worked in, and how to contact me.<br />
+</p>
