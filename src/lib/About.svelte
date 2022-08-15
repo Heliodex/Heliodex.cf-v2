6313033e903f4f224814f752ccaa23223f1dfa7a
Lewin Kelly <KEllyLewin2016@Gmail.com>
2022-08-15 23:01:05 +0100

    Reformat some pages and add animation

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -1,20 +1,26 @@
-<h1>About</h1>
-<br />
+<script lang="ts">
+	import { fly } from "svelte/transition"
+</script>
 
-<p>Heliodex</p>
-<br />
-<p>/'hiliə</p>
-<p class="ipa">ʊ</p>
-<p>d</p>
-<p class="ipa">ɛ</p>
-<p>ks/</p>
-<br />
-<!-- Mix and match letters with the nearest looking font for phonetic characters. Not a good way of doing it. -->
+<div in:fly={{ y: 40 }}>
+	<h1>About</h1>
+	<br />
 
-<p>
-	text go here<br />
-	todo: about page
-</p>
+	<p>Heliodex</p>
+	<br />
+	<p>/'hiliə</p>
+	<p class="ipa">ʊ</p>
+	<p>d</p>
+	<p class="ipa">ɛ</p>
+	<p>ks/</p>
+	<br />
+	<!-- Mix and match letters with the nearest looking font for phonetic characters. Not a good way of doing it. -->
+
+	<p>
+		text go here<br />
+		todo: about page
+	</p>
+</div>
 
 <style lang="sass">
 	.ipa
--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -1,6 +1,12 @@
-<h1>Heliodex.cf</h1>
-<p>
-	<br />
-	Welcome to Heliodex.cf!<br />
-	This is my portfolio page where you can see the projects that I've worked on, the teams that I've worked in, and how to contact me.<br />
-</p>
+<script lang="ts">
+	import { fly } from "svelte/transition"
+</script>
+
+<div in:fly={{ y: 40 }}>
+	<h1>Heliodex.cf</h1>
+	<p>
+		<br />
+		Welcome to Heliodex.cf!<br />
+		This is my portfolio page where you can see the projects that I've worked on, the teams that I've worked in, and how to contact me.<br />
+	</p>
+</div>
