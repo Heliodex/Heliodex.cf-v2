0e9c9e1c3b7a4fee7e428d03279c7bf68c76acf7
Lewin Kelly <heli@odex.cf>
2022-12-18 01:10:19 +0000

    Add eChat to projects page

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -13,21 +13,56 @@
 		<!-- Left column -->
 		<div class="column">
 			<div>
+				<h2>eChat</h2>
+
+				<p>
+					eChat is a messaging application, built as the successor to a previous project, DocSocial, and also designed to be used during school without getting caught. It was built with <a href="https://svelte.dev/" target="_blank" rel="noopener noreferrer">Svelte</a> and <a href="https://centrifugal.dev/" target="_blank" rel="noopener noreferrer">Centrifugo</a>.<br />
+					<br />
+					The app was made to work in a bookmarklet, and the popup would appear on the page upon clicking the bookmark.<br />
+					<br />
+					The original server will likely no longer be online, but both server and client have been open-sourced at <a href="https://github.com/Heliodex/eChat" target="_blank" rel="noopener noreferrer">github.com/Heliodex/eChat</a>.<br />
+					<a href="https://echat.cf/" target="_blank" rel="noopener noreferrer">echat.cf</a><br />
+					<br />
+
+					<img src="echat.png" alt="Screenshot of an eChat popup" width="400" />
+				</p>
+			</div>
+
+			<div>
 				<h2>DocSocial</h2>
 
 				<p>
-					Docsocial is a free, simple messaging application. It is designed to appear like a Google document from a quick glance, or from a look at your browser history.<br />
+					DocSocial is a free, simple messaging application. It is designed to appear like a Google document from a quick glance, or from a look at your browser history.<br />
 					<br />
 					DocSocial uses
-					<a class="realLink" href="https://Scaledrone.com/" target="_blank" rel="noopener noreferrer">Scaledrone</a>
+					<a href="https://scaledrone.com/" target="_blank" rel="noopener noreferrer">Scaledrone</a>
 					for its backend and for transferring messages.<br />
 					<br />
 					It was designed to work best with one specific device and OS, our school-assigned chromebooks, so pupils could chat with others in class and not be caught by a teacher.<br />
-					<a class="realLink" href="https://docsocial.tk/" target="_blank" rel="noopener noreferrer">DocSocial.tk</a><br />
-					<a class="realLink" href="https://docsocial.cf/" target="_blank" rel="noopener noreferrer">DocSocial.cf</a><br />
+					<a href="https://docsocial.tk/" target="_blank" rel="noopener noreferrer">docsocial.tk</a><br />
+					<a href="https://docsocial.cf/" target="_blank" rel="noopener noreferrer">docsocial.cf</a><br />
 					<br />
 
 					<img src="docsocial.png" alt="DocSocial Logo" width="300" />
+				</p>
+			</div>
+
+			<div>
+				<h2>Crazy Calculations</h2>
+
+				<p>
+					Crazy Calculations is a simple maths game that tests you through 10 levels of increasingly difficult maths questions.<br />
+					It was programmed in about a week for a Computer Science class assignment, where a video had to be recorded explaining the project.<br />
+					The project was started on 23 September 2021, and submitted on 4 October 2021.<br />
+					<br />
+					The game was inspired by (and much of the code ported from)
+					<a href="https://www.tomscott.com/usvsth3m/maths/" target="_blank" rel="noopener noreferrer">You Can't Do Simple Maths Under Pressure</a>
+					by UsVsTh3m.<br />
+					<br />
+					Crazy Calculations is playable at
+					<a href="https://www.roblox.com/games/7557207546/" target="_blank" rel="noopener noreferrer">Roblox.com/games/7557207546</a>.<br />
+					<br />
+					<img src="crazycalc.png" alt="Screenshot of the game" width="300" />
 				</p>
 			</div>
 
@@ -41,33 +76,14 @@
 					This was created by me in a few days, after I realised how long it would take to calculate all the range values manually.<br />
 					I remade the site a few months later, being my first time using Svelte.<br />
 					You can find the calculator at
-					<a class="realLink" href="https://pfcalc.heliodex.cf/" target="_blank" rel="noopener noreferrer">PFCalc.Heliodex.cf</a>
+					<a href="https://pfcalc.heliodex.cf/" target="_blank" rel="noopener noreferrer">pfcalc.heliodex.cf</a>
 					.<br />
 					The older version can be seen at
-					<a class="realLink" href="https://oldpfcalc.heliodex.cf/" target="_blank" rel="noopener noreferrer">OldPFCalc.Heliodex.cf</a>
+					<a href="https://oldpfcalc.heliodex.cf/" target="_blank" rel="noopener noreferrer">oldpfcalc.heliodex.cf</a>
 					.<br />
 					<br />
 
 					<img src="pfcalc.png" alt="Screenshot of the calculator page" width="300" />
-				</p>
-			</div>
-
-			<div>
-				<h2>Crazy Calculations</h2>
-
-				<p>
-					Crazy Calculations is a simple maths game that tests you through 10 levels of increasingly difficult maths questions.<br />
-					It was programmed in about a week for a Computer Science class assignment, where a video had to be recorded explaining the project.<br />
-					The project was started on 23 September 2021, and submitted on 4 October 2021.<br />
-					<br />
-					The game was inspired by (and much of the code ported from)
-					<a class="realLink" href="https://www.tomscott.com/usvsth3m/maths/" target="_blank" rel="noopener noreferrer">You Can't Do Simple Maths Under Pressure</a>
-					by UsVsTh3m.<br />
-					<br />
-					Crazy Calculations is playable at
-					<a class="realLink" href="https://www.roblox.com/games/7557207546/" target="_blank" rel="noopener noreferrer">Roblox.com/games/7557207546</a>.<br />
-					<br />
-					<img src="crazycalc.png" alt="Screenshot of the game" width="300" />
 				</p>
 			</div>
 		</div>
@@ -79,32 +95,12 @@
 
 				<p>
 					The website you're browsing right now!<br />
-					The current version of the website is a single-page app using the <a class="realLink" href="https://svelte.dev/" target="_blank" rel="noopener noreferrer">Svelte</a> framework.<br />
+					The current version of the website is a single-page app using <a href="https://svelte.dev/" target="_blank" rel="noopener noreferrer">Svelte</a>.<br />
 					<br />
-					The website's source code can be found at <a class="realLink" href="https://github.com/Heliodex/Heliodex.cf" target="_blank" rel="noopener noreferrer">Github.com/Heliodex/Heliodex.cf</a>.<br />
-					Older versions of the website can be seen at <a class="realLink" href="https://old.heliodex.cf/" target="_blank" rel="noopener noreferrer">Old.Heliodex.cf</a>.<br />
+					The website's source code can be found at <a href="https://github.com/Heliodex/Heliodex.cf" target="_blank" rel="noopener noreferrer">github.com/Heliodex/Heliodex.cf</a>.<br />
+					Older versions of the website can be seen at <a href="https://old.heliodex.cf/" target="_blank" rel="noopener noreferrer">old.heliodex.cf</a>.<br />
 					<br />
 					<iframe title="This website" src="/" width="100%" height="500px" frameborder="0" />
-				</p>
-			</div>
-
-			<div>
-				<h2>HybridOS</h2>
-
-				<p>
-					HybridOS was an open-source Python3 'meta-program'.<br />
-					I avoid calling it an operating system, because it's not an operating system, it was originally written in Python.<br />
-					<br />
-					HybridOS was worked on from mid- to late-2019 as a fusion of two similar programs, BlakBird by me, and Vortex by Taskmanager. (<a class="realLink" href="https://taski.ml/" target="_blank" rel="noopener noreferrer">taski.ml</a>)<br />
-					The program has a total line count of more than 1,100, and had features such as a multi-user signup and login system, a number of games and useful applications, plenty of colourful ASCII art, and a root admin system.<br />
-					HybridOS was ported to many different languages, including C, C++, Java, C# (twice), and Batch. Even linux distros were made as well!<br />
-					<br />
-					HybridOS was largely abandoned after the beginning of 2020, and the code for the first Python version has been released under The Unlicense on my Github page.<br />
-					<a class="realLink" href="https://github.com/HelioDex/HybridOS/" target="_blank" rel="noopener noreferrer">Github.com/Heliodex/HybridOS</a>
-					<br />
-					<a class="realLink" href="https://www.youtube.com/watch?v=QwhaqpOL310/" target="_blank" rel="noopener noreferrer">Something big may be coming soon, though...</a><br />
-					<br />
-					<img src="hylogos.png" alt="6 different HybridOS Logos throughout the versions" width="300" />
 				</p>
 			</div>
 
@@ -117,9 +113,30 @@
 					<br />
 					I cannot guarantee the accuracy of these programs, but while they <em>should</em> be decently accurate, please submit an issue to the repo if you find an error in any of the programs.<br />
 					They are available at
-					<a class="realLink" href="https://github.com/Heliodex/PythonCalculators/" target="_blank" rel="noopener noreferrer">Github.com/Heliodex/PythonCalculators</a>.<br />
+					<a href="https://github.com/Heliodex/PythonCalculators/" target="_blank" rel="noopener noreferrer">github.com/Heliodex/PythonCalculators</a>.<br />
 					<br />
-					<img src="pythoncollection.png" alt="A list of several of the Python programs" class="wide"/>
+					<img src="pythoncollection.png" alt="A list of several of the Python programs" class="wide" />
+				</p>
+
+			</div>
+			
+			<div>
+				<h2>HybridOS</h2>
+
+				<p>
+					HybridOS was an open-source Python3 'meta-program'.<br />
+					I avoid calling it an operating system, because it's not an operating system, it was originally written in Python.<br />
+					<br />
+					HybridOS was worked on from mid- to late-2019 as a fusion of two similar programs, BlakBird by me, and Vortex by Taskmanager. (<a href="https://taski.ml/" target="_blank" rel="noopener noreferrer">taski.ml</a>)<br />
+					The program has a total line count of more than 1,100, and had features such as a multi-user signup and login system, a number of games and useful applications, plenty of colourful ASCII art, and a root admin system.<br />
+					HybridOS was ported to many different languages, including C, C++, Java, C# (twice), and Batch. Even linux distros were made as well!<br />
+					<br />
+					HybridOS was largely abandoned after the beginning of 2020, and the code for the first Python version has been released on my Github page.<br />
+					<a href="https://github.com/HelioDex/HybridOS/" target="_blank" rel="noopener noreferrer">github.com/Heliodex/HybridOS</a>
+					<br />
+					<a href="https://www.youtube.com/watch?v=QwhaqpOL310/" target="_blank" rel="noopener noreferrer">Something big may be coming soon, though...</a><br />
+					<br />
+					<img src="hylogos.png" alt="6 different HybridOS Logos throughout the versions" width="300" />
 				</p>
 			</div>
 		</div>
