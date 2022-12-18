8ef2014c5643b76b61ab9efc48d57ac1f97c2968
Lewin Kelly <heli@odex.cf>
2022-12-18 01:33:16 +0000

    Update contact page links

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -9,21 +9,24 @@
 	<p>
 		Here's a list of a few of the websites/media that you are able to contact me via.<br /><br />
 		Github: @Heliodex,
-		<a class="realLink" href="https://github.com/Heliodex/" target="_blank" rel="noopener noreferrer">Github.com/Heliodex</a><br />
+		<a href="https://github.com/Heliodex/" target="_blank" rel="noopener noreferrer">github.com/Heliodex</a><br />
 		Twitter: @lwinklly,
-		<a class="realLink" href="https://twitter.com/lwinklly/" target="_blank" rel="noopener noreferrer">Twitter.com/lwinklly</a><br />
+		<a href="https://twitter.com/lwinklly/" target="_blank" rel="noopener noreferrer">twitter.com/lwinklly</a><br />
+		Youtube: @lwinklly,
+		<a href="https://www.youtube.com/@lwinklly/" target="_blank" rel="noopener noreferrer">youtube.com/@lwinklly</a><br />
 		Reddit: u/Heliodex,
-		<a class="realLink" href="https://www.reddit.com/user/Heliodex/" target="_blank" rel="noopener noreferrer">Reddit.com/u/Heliodex</a><br />
+		<a href="https://www.reddit.com/user/Heliodex/" target="_blank" rel="noopener noreferrer">reddit.com/u/Heliodex</a><br />
 		Developer Forum: Lewin4,
-		<a class="realLink" href="https://devforum.roblox.com/u/lewin4/summary/" target="_blank" rel="noopener noreferrer">Devforum.Roblox.com/u/lewin4</a><br />
+		<a href="https://devforum.roblox.com/u/lewin4/summary/" target="_blank" rel="noopener noreferrer">devforum.roblox.com/u/lewin4</a><br />
 		Roblox: @Lewin4,
-		<a class="realLink" href="https://www.roblox.com/users/77663253/profile/" target="_blank" rel="noopener noreferrer">Roblox.com/users/77663253/profile</a><br />
-		Youtube: @Heliodex,
-		<a class="realLink" href="https://www.youtube.com/channel/UC8E2hw963cW5IJXZnBE6_vg/" target="_blank" rel="noopener noreferrer">YouTube.com/channel/UC8E2hw963cW5IJXZnBE6_vg</a><br />
+		<a href="https://www.roblox.com/users/77663253/profile/" target="_blank" rel="noopener noreferrer">roblox.com/users/77663253/profile</a><br />
+		StackOverflow: Heliodex,
+		<a href="https://stackoverflow.com/users/12576382/" target="_blank" rel="noopener noreferrer">stackoverflow.com/users/12576382</a><br />
 		Discord: Heliodex#3590,
-		<a class="realLink" href="https://discord.com/users/290622468547411968" target="_blank" rel="noopener noreferrer">Discord.com/users/290622468547411968</a><br />
-		Telegram: @Heliodex<br />
+		<a href="https://discord.com/users/290622468547411968" target="_blank" rel="noopener noreferrer">discord.com/users/290622468547411968</a><br />
+		Telegram: @Heliodex,
+		<a href="https://t.me/heliodex" target="_blank" rel="noopener noreferrer">t.me/heliodex</a><br />
 		Email: Heli@odex.cf,
-		<a class="realLink" href="mailto:Heli@odex.cf" target="_blank" rel="noopener noreferrer">Heli@odex.cf</a><br />
+		<a href="mailto:Heli@odex.cf" target="_blank" rel="noopener noreferrer">heli@odex.cf</a><br />
 	</p>
 </div>
