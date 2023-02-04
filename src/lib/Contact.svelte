b0959e75d4024e8e96e5f6952ff3d061dcdb798b
Lewin Kelly <heli@odex.cf>
2023-02-04 21:18:19 +0000

    Add table layout to links on contact page

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -6,27 +6,90 @@
 	<h1>Contact</h1>
 	<br />
 
-	<p>
-		Here's a list of a few of the websites/media that you are able to contact me via.<br /><br />
-		Github: @Heliodex,
-		<a href="https://github.com/Heliodex/" target="_blank" rel="noopener noreferrer">github.com/Heliodex</a><br />
-		Twitter: @lwinklly,
-		<a href="https://twitter.com/lwinklly/" target="_blank" rel="noopener noreferrer">twitter.com/lwinklly</a><br />
-		Youtube: @lwinklly,
-		<a href="https://www.youtube.com/@lwinklly/" target="_blank" rel="noopener noreferrer">youtube.com/@lwinklly</a><br />
-		Reddit: u/Heliodex,
-		<a href="https://www.reddit.com/user/Heliodex/" target="_blank" rel="noopener noreferrer">reddit.com/u/Heliodex</a><br />
-		Developer Forum: Lewin4,
-		<a href="https://devforum.roblox.com/u/lewin4/summary/" target="_blank" rel="noopener noreferrer">devforum.roblox.com/u/lewin4</a><br />
-		Roblox: @Lewin4,
-		<a href="https://www.roblox.com/users/77663253/profile/" target="_blank" rel="noopener noreferrer">roblox.com/users/77663253/profile</a><br />
-		StackOverflow: Heliodex,
-		<a href="https://stackoverflow.com/users/12576382/" target="_blank" rel="noopener noreferrer">stackoverflow.com/users/12576382</a><br />
-		Discord: Heliodex#3590,
-		<a href="https://discord.com/users/290622468547411968" target="_blank" rel="noopener noreferrer">discord.com/users/290622468547411968</a><br />
-		Telegram: @Heliodex,
-		<a href="https://t.me/heliodex" target="_blank" rel="noopener noreferrer">t.me/heliodex</a><br />
-		Email: Heli@odex.cf,
-		<a href="mailto:Heli@odex.cf" target="_blank" rel="noopener noreferrer">heli@odex.cf</a><br />
-	</p>
+	<p>Here's a list of a few of the websites/media that you are able to contact me via.</p>
+	<br />
+	<br />
+	<table>
+		<tr>
+			<td>Github</td>
+			<td>@Heliodex</td>
+			<td>
+				<a href="https://github.com/Heliodex/" target="_blank" rel="noopener noreferrer">github.com/Heliodex</a>
+			</td>
+		</tr>
+		<tr>
+			<td>Twitter</td>
+			<td>@lwinklly</td>
+			<td>
+				<a href="https://twitter.com/lwinklly/" target="_blank" rel="noopener noreferrer">twitter.com/lwinklly</a>
+			</td>
+		</tr>
+		<tr>
+			<td>Youtube</td>
+			<td>@lwinklly</td>
+			<td>
+				<a href="https://youtube.com/@lwinklly/" target="_blank" rel="noopener noreferrer">youtube.com/@lwinklly</a>
+			</td>
+		</tr>
+		<tr>
+			<td>Reddit</td>
+			<td>u/Heliodex</td>
+			<td>
+				<a href="https://reddit.com/user/Heliodex/" target="_blank" rel="noopener noreferrer">reddit.com/u/Heliodex</a>
+			</td>
+		</tr>
+		<tr>
+			<td>Developer Forum</td>
+			<td>@Lewin4</td>
+			<td>
+				<a href="https://devforum.roblox.com/u/lewin4/summary/" target="_blank" rel="noopener noreferrer">devforum.roblox.com/u/lewin4</a>
+			</td>
+		</tr>
+		<tr>
+			<td>Roblox</td>
+			<td>@Lewin4</td>
+			<td>
+				<a href="https://roblox.com/users/77663253/profile/" target="_blank" rel="noopener noreferrer">roblox.com/users/77663253/profile</a>
+			</td>
+		</tr>
+		<tr>
+			<td>StackOverflow</td>
+			<td>Heliodex</td>
+			<td>
+				<a href="https://stackoverflow.com/users/12576382/" target="_blank" rel="noopener noreferrer">stackoverflow.com/users/12576382</a>
+			</td>
+		</tr>
+		<tr>
+			<td>Discord</td>
+			<td>Heliodex#3590</td>
+			<td>
+				<a href="https://discord.com/users/290622468547411968" target="_blank" rel="noopener noreferrer">discord.com/users/290622468547411968</a>
+			</td>
+		</tr>
+		<tr>
+			<td>Telegram</td>
+			<td>@Heliodex</td>
+			<td>
+				<a href="https://t.me/heliodex" target="_blank" rel="noopener noreferrer">t.me/heliodex</a>
+			</td>
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td> Heli@odex.cf </td>
+			<td>
+				<a href="mailto:heli@odex.cf" target="_blank" rel="noopener noreferrer">heli@odex.cf</a>
+			</td>
+		</tr>
+	</table>
 </main>
+
+<style lang="sass">
+	td
+		padding-right: 1rem
+
+	@media screen and (max-width: 480px)
+		td
+			display: block
+		tr
+			height: 5rem
+</style>
