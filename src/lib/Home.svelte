99c727431d5f426401ef71efffbc1c18e6797d53
Lewin Kelly <heli@odex.cf>
2023-02-04 18:33:53 +0000

    Formatting fixes

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -2,11 +2,11 @@
 	import { fly } from "svelte/transition"
 </script>
 
-<div in:fly={{ y: 40 }}>
+<main in:fly={{ y: 40 }}>
 	<h1>Heliodex.cf</h1>
 	<p>
 		<br />
 		Welcome to Heliodex.cf!<br />
 		This is my portfolio page where you can see the projects that I've worked on, the teams that I've worked in, and how to contact me.<br />
 	</p>
-</div>
+</main>
